8aa88964f6c6a508dbda3bf834b0aca097f61ba5
gao <gaozh51@163.com>
2017-07-27 00:05:32 +0800

    add index

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 
 from .models import Post
 
-
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','pub_date')
+admin.site.register(Post,PostAdmin)
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Post
+from django.template.loader import get_template
+from datetime import datetime
 # Create your views here.
+
+
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+
+    post_lists = list()
+    for count,post in enumerate(posts):
+        post_lists.append("No.{}:".format(str(count))+str(post.title))
+        # post_lists.append("<small>"+str(post.body)+"</small><br><br>")
+    html = template.render(locals())
+    return HttpResponse(html)
